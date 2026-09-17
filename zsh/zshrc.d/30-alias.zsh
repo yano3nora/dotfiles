6ea99210ba014df637f155b33194479e07fe5387
yano3nora <18728262+yano3nora@users.noreply.github.com>
2026-09-17 14:17:01 +0900

    fix: cd alias 追加

--- zsh/zshrc.d/30-alias.zsh
+++ zsh/zshrc.d/30-alias.zsh
@@ -8,6 +8,8 @@ alias rm="trash"
 alias reload="exec $SHELL -l"
 alias chrome='open -a "Google Chrome"'
 alias cdw="cd ~/Downloads"
+alias cdg="cd ~/git"
+alias cdd="cd ~/git/yano3nora/dotfiles"
 
 # coda - https://github.com/yano3nora/coda
 # 意図: git commit など `$EDITOR` を実行するツールでも coda を使う。
